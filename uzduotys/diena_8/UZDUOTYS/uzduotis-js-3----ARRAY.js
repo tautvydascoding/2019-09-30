2b9d585f34dd84972685a121e1324df42c7372cf
Tautvydas <tautvydascoding@gmail.com>
2019-10-08 08:21:02 +0300

    8 day

---
+++ uzduotys/diena_8/UZDUOTYS/uzduotis-js-3----ARRAY.js
@@ -0,0 +1,40 @@
+// ========js best practice & common mistakes (~2h)
+
+
+// -------- --Array / Masyvai  Methods (~2h)--------------------
+names = ["Tomas", "Pietkus", 25, "VDU - inzinerija"];
+
+console.log(names);
+// UZDUOTIS
+// pa'googlinti "javascript array Methods":  https://www.w3schools.com/js/js_array_methods.asp
+// ismeginti visas komandas/ metodus
+// metodas - jau esama js f-ja, kuria suprogramavo js kalbos kurejai
+
+// metodai:
+// .shift()
+// .length
+// .join()
+// .pop()
+// .slice()
+// ...
+// pvz    names.shift("Tomas");
+
+// =========================================================
+// SAVARANKISKA UZDUOTIS  Array
+keliautojas = ["Tomas", "Pietkus", 25, "VDU - inzinerija"];
+
+
+// 1. i masyvo gala ideti elmenta "butelis vandens"
+// 2. i masyvo pradzia ideti elmenta "ziebtuvelis"
+// 3. istrinti 3-cia elmenta
+
+// --------GERESNI UZDAVINIAI:
+// 4. sukurti masyva (50 ilgumo) naudojant FOR ir uzpildyti visa skaiciais "0" (kiekvienas stalcius turi but lygus "0");
+// 5.1  sukurti masyva naudojant WHILE (50 ilgumo) ir uzpildyti visa skaiciumi 1;
+// 5.2: kas antra ^ masyvo elementa pakeisti i "3"
+// [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, ....
+// 5.3: kas 5-ta ^ masyvo elementa pakeisti i "9"
+// [1, 3, 1, 3, 9, 3, 1, 3, 1, 9, ....
+
+
+//=============bootstrap---sm-md-lg-xl (30-60m)==========
---
+++ uzduotys/diena_8/UZDUOTYS/uzduotis-js-4---IF-pasikartoti.js
@@ -0,0 +1,29 @@
+// 1 UZDUOTIS
+// let zmg = ["Tomas", 21, "Kaunas", 820, true];
+//
+// patikrinti ar zmogus vedes :
+// A) naudoti if  ir "=="
+// jeigu vedes - isvesti teksta : "Sveikinu - gausite paskola"
+// jeigu nevedes - isvesti teksta: "Deja, jums paskola nepriklauso"
+
+// B) sunkesnis: patikrinima uz uzrasyti trumpesniu budu be "=="
+// C) sunkesnis: perfrazuoti tikrinima
+//  ir ji uzrasyti su neiginiu: "!="
+
+// 3 UZDUOTIS
+
+// 1) // sukurti kintamuosius:
+ // arRodyti =   (galimi variantai true/ false)
+ // vartotojoTipas =   "admin";
+ // userName = "Tomas";
+
+// 1.2) parasyti "if", kuris pagal kintamojo reiksme "arRodyti"  :  jeigu "true" isvestu - "Labas Tomai"
+// jeigu "false" isvestu : "Sveiki atvykia!"
+
+//  1.3 Jeigu vartotojo tipas 'admin': isvesti ne tik pasisveikinima, bet ir koki nors paveiksliuka
+
+// uzduotis 4
+// 1.1 susikurti tuscia masyva
+// 1.2 uzpildyti masyva "1" (50 stalciu)
+// 1.3 parasyti IF, kuris tikrina ar stalciaus nr. lyginis, jei lyginis - stalciaus turini pakeicia i zodi "lyginis"
+// 
