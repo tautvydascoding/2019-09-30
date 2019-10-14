af87ebeecd1dfeda3fb1ec6cf13017509532562c
Asta Urbonaviciene <aurbonaviciene@gmail.com>
2019-10-14 11:18:35 +0300

    10.14

---
+++ dirbame_cia/diena_11/asta/[default-tuscias-porjektas/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- mano css failas visada pats zemiausias -->
+        <link rel="stylesheet"  href="css/style.css">
+    </head>
+    <body>
+        <h1>Isijunk console (nars)</h1>
+
+        <?php
+        $vardas = 'tomas';
+        $vardas1pirmuno = '';
+        $vardas_pirmuno = '';
+        $tekstas1 = "Raudonkepuraite ejo ir sutiko $vardas apsizodziavo";
+        $tekstas2 = 'Raudonkepuraite ejo ir sutiko $vardas apsizodziavo';
+        echo $tekstas1; //atspausdinimas
+        echo "$tekstas1"; //atspausdinimas tas pats kas ir virsutine eilute
+        echo "$tekstas2";
+        echo "Tomas";
+        $masyvas = [];
+
+
+        ?>
+
+        <?php ?>
+
+        <!-- mano js failas visada pats zemiausias -->
+        <script type="text/javascript" src='main.js'> </script>
+    </body>
+</html>
