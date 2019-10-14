309ada0385596ff8c5062214b699c0c4d0ee4ff8
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-10-14 09:38:59 +0300

    pataisyta uzduotis

--- uzduotys/diena_11/UZDUOTYS/2.1 uzduotis-PHP-inlude-preke-kintamieji+++++.php
+++ uzduotys/diena_11/UZDUOTYS/2.1 uzduotis-PHP-inlude-preke-kintamieji+++++.php
@@ -13,13 +13,13 @@ index.php
 // button (Kaina)
 
 // 2) sukurti index.php su "<!Doctype html body" (ideti container ir row su bootatrap)
-// 2) index.php faile - paleisti FOR cikla 6 kartus include('template-preke.php');
 
-// 3) index.php
+// 2.2) index.php
 // sukurti kintamuosius:
 // $kaina = ...;
 // $antraste = ...;
 // $aprasymas = ...;
-// 4) template-preke.php faile   isvesti kintamuosius $kaina, $antraste, $aprasymas
+// 3) template-preke.php faile   isvesti kintamuosius $kaina, $antraste, $aprasymas
 pvz
 <h2> <?php echo $antraste;  ?> </h2>
+// 3.2) index.php faile!! - paleisti FOR cikla 6 kartus include('template-preke.php');
