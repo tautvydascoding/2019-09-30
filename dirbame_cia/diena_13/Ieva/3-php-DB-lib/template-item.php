a0d10d4bdd3dad1dffe019b0752aa25dc30e1a49
gajaIeva <gaja.ieva@gmail.com>
2019-10-17 09:20:00 +0300

    14 dienos darbai. SASS ir PHP

---
+++ dirbame_cia/diena_13/Ieva/3-php-DB-lib/template-item.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h2 class='bg-info col-md-4  m-1'> {$gyd ['name']} {$gyd ['lname']} </h2>";
+
+?>
+
+<!-- <h2 class='bg-info col-md-4  m-1'>
+    <?= $gyd ['name'] .$gyd ['lname'] ?>
+</h2> -->
