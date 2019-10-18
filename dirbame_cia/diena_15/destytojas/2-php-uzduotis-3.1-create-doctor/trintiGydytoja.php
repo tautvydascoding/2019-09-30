499b7ca4b3af5bcd48906bf251b6919c380a58b0
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-10-18 12:29:30 +0300

    visi isvedimas gydytojai klases darbas

---
+++ dirbame_cia/diena_15/destytojas/2-php-uzduotis-3.1-create-doctor/trintiGydytoja.php
@@ -0,0 +1,24 @@
+<h1>Tikriausiai sekmingai istrintas   gydytojas</h1>
+
+<?php
+    include('db_functions.php');
+    // print_r($_GET); // test
+    $x = $_GET['numeris'];
+    deleteDoctor($x);
+
+
+
+ ?>
+<h2>Sekmingai istrinta!!!!</h2>
+
+
+<!-- js redirect: -->
+<script type="text/javascript">
+    //window.location.replace("http://example.com/");
+</script>
+
+<?php
+// PHP redirect
+// header("Location: http://example.com/myOtherPage.php");
+// die();
+ ?>
