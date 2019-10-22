c5f4a65eeee6a75832fafc05ac59b6b964385fd0
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-10-22 11:24:15 +0300

    uzduotis 3

---
+++ dirbame_cia/diena_17/tautvydas/2-php-class-OOP-objektinis-programavimas/automobilis.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+
+
+/**
+ *
+ */
+class Automobilis  {
+    public $spalva = "nezinome";
+
+    public function vaziuoti() {
+        echo "$this->spalva  automobilis vaziuoja gatve <br />";
+    }
+}
