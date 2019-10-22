5e6b5ac54d288fe8da32e1728a41bd7db93b88c3
tautvydascoding@gmail.com <Tautvydascoding@gmail.com>
2019-10-22 15:12:35 +0300

    uzduotys pasikaroti Ciklu testui

--- dirbame_cia/diena_17/tautvydas/test.php
+++ dirbame_cia/diena_17/tautvydas/test.php
@@ -1,1 +1,7 @@
+
+<form class="" action="index.html" method="post">
+    <input type="text" name="vardas" value="<?php if(!$publish_mode) { echo 'TOmas';} ?>">
+    <input type="text" name="pavarde" value="Tomauskis">
+</form>
+
 <?php
